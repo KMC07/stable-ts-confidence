0a2eb91657f29c09c5c89a63b9f8153077ab9a34
KMC07 <ojiakokamsi@gmail.com>
2023-01-12 19:25:10 +0000

    Fixed setup issues

--- setup.py
+++ setup.py
@@ -19,11 +19,11 @@ setup(
     long_description_content_type='text/markdown',
     python_requires=">=3.7",
     author="Vit",
-    url="https://github.com/Anoncheg1/stable-ts-con",
+    url="https://github.com/KMC07/stable-ts-confidence",
     license="MIT",
     packages=['stable_whisper'],
     install_requires=[
-      "git+https://github.com/openai/whisper.git"
+      "whisper @ git+https://github.com/Blair-Johnson/batch-whisper.git"
     ],
     include_package_data=False
 )
